28c2e4e7e0a915a65223fceb2f37cd90369fa457
silvaed <edsonbr27@gmail.com>
2019-03-12 06:43:22 +0000

    More Formatting tasks

--- S00190492_Creche/Model/Student.cs
+++ S00190492_Creche/Model/Student.cs
@@ -19,8 +19,8 @@ namespace S00190492_Creche.Model
         [Required(ErrorMessage = "The PPS number is required")]
         [Display(Name = "PPS Number")]
         [Key]
-        public string PPS_ID { get; set; } = "";
-        [Required(ErrorMessage = "Please  Enter A Date")]
+        public string PPS { get; set; } = "";
+        [Required(ErrorMessage = "Please  Enter a birth date")]
         [Display(Name = "Date of Birth")]
         public DateTime DateOfBirth { get; set; }
         [Required(ErrorMessage = "Select a gender")]
@@ -32,40 +32,40 @@ namespace S00190492_Creche.Model
         [Required(ErrorMessage = "The parent last name is required")]
         [Display(Name = "Parent Last Name")]
         public string ParentLastName { get; set; } = "";
-        //[Required(ErrorMessage = "Select a relationship")]
-        //[Display(Name = "Relationship")]
-        //public string[] RelationshipToChild { get; set; }
-        //[Required(ErrorMessage = "The address is required")]
-        //[Display(Name = "Address")]
-        //public string Address { get; set; } = "";
-        //[Required(ErrorMessage = "The city is required")]
-        //[Display(Name = "City")]
-        //public string City { get; set; } = "";
-        //[Required(ErrorMessage = "The ZipCode is required")]
-        //[Display(Name = "ZipCode")]
-        //public string ZipCode { get; set; } = "";
-        //[Required(ErrorMessage = "The mobile number is required")]
-        //[Display(Name = "Mobile Number")]
-        //[RegularExpression(@"0\d{9}", ErrorMessage = "The Phone Number should have the format 0XXXXXXXXX Whitout spaces")]
-        //public string MobileNumber { get; set; } = "";
-        //[Display(Name = "Second Contact Number")]
-        //public string SecondContactNumber { get; set; } = "";
-        //[Required(ErrorMessage = "The e-mail is required")]
-        //[Display(Name = "E-mail")]
-        //[DataType(DataType.EmailAddress)]
-        //public string Email { get; set; }
-        //[Display(Name = "Second E-mail")]
-        //[DataType(DataType.EmailAddress)]
-        //public string SecondEmail { get; set; }
-        //[Required(ErrorMessage = "Select Full or Part Time")]
-        //[Display(Name = "Full or Part Time")]
-        //public string[] Hours { get; set; } = { "Full Time", "Part Time" };
+        [Required(ErrorMessage = "Select a relationship")]
+        [Display(Name = "Relationship")]
+        public string RelationshipToChild { get; set; }
+        [Required(ErrorMessage = "The address is required")]
+        [Display(Name = "Address")]
+        public string Address { get; set; } = "";
+        [Required(ErrorMessage = "The city is required")]
+        [Display(Name = "City")]
+        public string City { get; set; } = "";
+        [Required(ErrorMessage = "The ZipCode is required")]
+        [Display(Name = "ZipCode")]
+        public string ZipCode { get; set; } = "";
+        [Required(ErrorMessage = "The mobile number is required")]
+        [Display(Name = "Mobile Number")]
+        [RegularExpression(@"0\d{9}", ErrorMessage = "The Phone Number should have the format 0XXXXXXXXX Whitout spaces")]
+        public string MobileNumber { get; set; } = "";
+        [Display(Name = "Second Contact Number")]
+        public string SecondContactNumber { get; set; } = "";
+        [Required(ErrorMessage = "The e-mail is required")]
+        [Display(Name = "E-mail")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Display(Name = "Second E-mail")]
+        [DataType(DataType.EmailAddress)]
+        public string SecondEmail { get; set; }
+        [Required(ErrorMessage = "Select Full or Part Time")]
+        [Display(Name = "Full or Part Time")]
+        public string Hours { get; set; }
         //[Required(ErrorMessage = "Select at least one day of week")]
         //[Display(Name = "Days of Week")]
-        //public string[] DaysRequested { get; set; } = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
-        //[Required(ErrorMessage = "The starting date is required")]
-        //[Display(Name = "Starting Date")]
-        //public DateTime StartingDate { get; set; }
+        //public string[] DaysRequested { get; set; } 
+        [Required(ErrorMessage = "The starting date is required")]
+        [Display(Name = "Starting Date")]
+        public DateTime StartingDate { get; set; }
 
         public Student()
         {
