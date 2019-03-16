0daaa860b6c3e5421588f3dbdc1c6a15a0945ec8
silvaed <edsonbr27@gmail.com>
2019-03-16 22:06:19 +0000

    Validation - Fomatting

--- S00190492_Creche/Pages/Student/Student_Information_Form.cshtml.cs
+++ S00190492_Creche/Pages/Student/Student_Information_Form.cshtml.cs
@@ -21,6 +21,68 @@ namespace S00190492_Creche.Pages.Student
         [BindProperty]
         public S00190492_Creche.Model.Student Student { get; set; }
 
+        public bool DaysOfWeek()
+        {
+            bool atLeastADay;
+            if (Student.Monday == true || Student.Tuesday == true || Student.Wednesday == true || Student.Thursday == true || Student.Friday == true)
+            {
+                if (Student.Monday) Student.DaysOfWeek += "Monday ";
+                if (Student.Tuesday) Student.DaysOfWeek += "Tuesday ";
+                if (Student.Wednesday) Student.DaysOfWeek += "Wednesday ";
+                if (Student.Thursday) Student.DaysOfWeek += "Thursday ";
+                if (Student.Friday) Student.DaysOfWeek += "Friday";
+                Student.CheckDaysOfWeek = "";
+                
+                if(Student.Hours == "Full Time")
+                {
+                    Student.TotalDue =TotalDays() * 35;
+                }
+                else
+                {
+                    Student.TotalDue = TotalDays() * 20;
+                }
+
+
+                atLeastADay = true;
+            }
+            else
+            {
+                Student.CheckDaysOfWeek = "You need to select at least a day of the week";
+                atLeastADay = false;
+            }
+
+
+            return atLeastADay;
+        }
+
+        public bool CheckDateStart()
+        {
+            bool checkDates = false;
+            if (Student.StartingDate > DateTime.Now)
+            {
+                Student.CheckDateStart = "";
+                checkDates = true;
+            }
+            else
+            {
+                Student.CheckDateStart = "The date is not valid";
+                checkDates = false;
+            }
+            return checkDates;
+        }
+
+        public int TotalDays()
+        {
+            int numberOfDays = 0;
+            if (Student.Monday) numberOfDays += 1;
+            if (Student.Tuesday) numberOfDays += 1;
+            if (Student.Wednesday) numberOfDays += 1;
+            if (Student.Thursday) numberOfDays += 1;
+            if (Student.Friday) numberOfDays += 1;
+
+            return numberOfDays;
+        }
+
         public void OnGet()
         {
 
@@ -28,11 +90,12 @@ namespace S00190492_Creche.Pages.Student
 
         public async Task<IActionResult> OnPostAsync()
         {
-            if(ModelState.IsValid && Student.Monday == true)
+            if(ModelState.IsValid && DaysOfWeek() && CheckDateStart() )
             {
+                
                 _db.Students.Add(Student);
                 await _db.SaveChangesAsync();
-                return RedirectToPage("ListStudents");
+                return RedirectToPage("ValueDetails");
 
             }
             else
---
+++ S00190492_Creche/Pages/Student/ValueDetails.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using S00190492_Creche.Model;
+
+namespace S00190492_Creche.Pages.Student
+{
+    public class ValueDetailsModel : PageModel
+    {
+        private readonly CrecheContext _db;
+
+        public ValueDetailsModel(CrecheContext db)
+        {
+            _db = db;
+        }
+
+        public IList<S00190492_Creche.Model.Student> Students { get; private set; }
+
+        public async Task OnGetAsync()
+        {
+            Students = await _db.Students.AsNoTracking().ToListAsync();
+        }
+       
+    }
+}
